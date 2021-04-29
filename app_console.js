a0b36d381c2abcadf1fcb60d41bba388d9276ef4
Rishabh <rishabhmaster1@gmail.com>
2021-04-29 18:25:10 +0530

    Add files via upload

---
+++ app_console.js
@@ -0,0 +1,25 @@
+// Log to console
+console.log('Hello World');
+console.log(123);
+console.log(true);
+var greeting = 'Hello';
+console.log(greeting);
+console.log([1,2,3,4]);
+console.log({a:1, b:2});
+
+console.table({a:1, b:2});
+console.error('This is some error'); 
+console.warn('This is a warning');
+
+console.clear();
+
+console.time('Hello');
+  console.log('This will show');
+  console.log('us how much');
+  console.log('time did it take');
+  console.log('to print all');
+  console.log('of this in');
+  console.log('the console');
+console.timeEnd('Hello');
+
+
